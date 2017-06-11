16876429b5b79cb36baf8be2f0bdc7020d390abd
matt11matthew <matt11matthew@gmail.com>
2017-06-11 17:36:13 -0400

    Holograms

--- src/main/java/com/riddlesvillage/core/internal/command/NameHistoryCommand.java
+++ src/main/java/com/riddlesvillage/core/internal/command/NameHistoryCommand.java
@@ -1,6 +1,5 @@
 package com.riddlesvillage.core.internal.command;
 
-import com.google.common.collect.ImmutableList;
 import com.riddlesvillage.core.Messaging;
 import com.riddlesvillage.core.RiddlesCore;
 import com.riddlesvillage.core.player.CorePlayer;
@@ -34,7 +33,7 @@ public class NameHistoryCommand implements CommandExecutor {
         switch (args.length) {
             case 0:
                 if (!RiddlesCore.logIf(!isPlayer, "command.only-players")) {
-                    ImmutableList<String> l = playerSender.getStatisticValues();
+                    List<String> l = playerSender.getNameHistory();
                     playerSender.sendMessages(l.toArray(new String[l.size()]));
                 }
                 break;
@@ -43,7 +42,7 @@ public class NameHistoryCommand implements CommandExecutor {
 
                 CorePlayer victim = MANAGER.get(victimName);
                 if (victim != null) {
-                    ImmutableList<String> l = victim.getStatisticValues();
+                    List<String> l = victim.getNameHistory();
                     playerSender.sendMessages(l.toArray(new String[l.size()]));
                 } else {
                     AbstractCoreProfile profile = OfflineCorePlayer.fromName(args[0]);
