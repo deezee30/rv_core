06a4eed7b35047ea9b0e224e5175bcca3d238397
Maulss <maulss@gmx.com>
2017-06-14 22:47:41 +0100

    revert scroller

--- src/main/java/com/riddlesvillage/core/util/Scroller.java
+++ src/main/java/com/riddlesvillage/core/util/Scroller.java
@@ -15,7 +15,6 @@ import java.util.List;
 public final class Scroller implements Serializable {
 
 	private static final long serialVersionUID = -3296965334328559328L;
-	private static final char COLOUR_CHAR = '&';
 
 	private String scroll = null;
 	private int position;
@@ -24,11 +23,10 @@ public final class Scroller implements Serializable {
 
 	/**
 	 * @param message      The String to scroll
-	 * @param width        The width of the window to scroll across (i.e. 16 for signs)
+	 * @param width        The width of the window to scroll across
 	 * @param spaceBetween The amount of spaces between each repetition
-	 * @param colourChar   The colour code character you're using (i.e. & or §)
 	 */
-	public Scroller(String message, int width, int spaceBetween, char colourChar) {
+	public Scroller(String message, int width, int spaceBetween) {
 		if (message.length() <= width) {
 			scroll = message;
 			return;
@@ -38,20 +36,16 @@ public final class Scroller implements Serializable {
 
 		if (message.length() < width) {
 			StringBuilder sb = new StringBuilder(message);
-			while (sb.length() < width)
-				sb.append(" ");
+			while (sb.length() < width) sb.append(" ");
 			message = sb.toString();
 		}
 
 		width -= 2;
 
-		if (width < 1)
-			width = 1;
-		if (spaceBetween < 0)
-			spaceBetween = 0;
+		if (width < 1) width = 1;
+		if (spaceBetween < 0) spaceBetween = 0;
 
-		if (colourChar != '§')
-			message = ChatColor.translateAlternateColorCodes(colourChar, message);
+		message = ChatColor.translateAlternateColorCodes('&', message);
 
 
 		for (int i = 0; i < message.length() - width; i++)
@@ -60,31 +54,29 @@ public final class Scroller implements Serializable {
 		StringBuilder space = new StringBuilder();
 		for (int i = 0; i < spaceBetween; ++i) {
 			list.add(message.substring(message.length() - width + (i > width ? width : i), message.length()) + space);
-			if (space.length() < width)
-				space.append(" ");
+			if (space.length() < width) space.append(" ");
 		}
 
 		for (int i = 0; i < width - spaceBetween; ++i)
 			list.add(message.substring(message.length() - width + spaceBetween + i, message.length()) + space + message.substring(0, i));
 
 		for (int i = 0; i < spaceBetween; i++) {
-			if (i > space.length())
-				break;
+			if (i > space.length()) break;
 			list.add(space.substring(0, space.length() - i) + message.substring(0, width - (spaceBetween > width ? width : spaceBetween) + i));
 		}
 	}
 
 	/**
-	 * @return Gets the next String to display
+	 * @return the next String to display
 	 */
 	public String next() {
 		if (scroll != null) return scroll;
 
 		StringBuilder sb = getNext();
-		if (sb.charAt(sb.length() - 1) == COLOUR_CHAR)
+		if (sb.charAt(sb.length() - 1) == '&')
 			sb.setCharAt(sb.length() - 1, ' ');
 
-		if (sb.charAt(0) == COLOUR_CHAR) {
+		if (sb.charAt(0) == '&') {
 			ChatColor c = ChatColor.getByChar(sb.charAt(1));
 			if (c != null) {
 				colour = c;
@@ -95,10 +87,9 @@ public final class Scroller implements Serializable {
 		}
 
 		return colour + sb.toString();
-
 	}
 
 	private StringBuilder getNext() {
-		return new StringBuilder(list.get(position++ % list.size()).substring(0));
+		return new StringBuilder(list.get(position++ % list.size()));
 	}
 }
